95299356cc8e7931f4f4acb4c2577bc28c98c1dd
Lucas Ou-Yang <lucasyangpersonal@gmail.com>
2014-08-01 01:07:23 -0700

    Refactor miscellaneous data files to more sensible location.

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -19,13 +19,13 @@ log = logging.getLogger(__name__)
 
 PARENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
-POPULAR_URLS = os.path.join(PARENT_DIRECTORY, 'data/popular_sources.txt')
-USERAGENTS = os.path.join(PARENT_DIRECTORY, 'data/useragents.txt')
+POPULAR_URLS = os.path.join(PARENT_DIRECTORY, 'resources/misc/popular_sources.txt')
+USERAGENTS = os.path.join(PARENT_DIRECTORY, 'resources/misc/useragents.txt')
 
 STOPWORDS_DIR = os.path.join(PARENT_DIRECTORY, 'resources/text')
 
 # NLP stopwords are != regular stopwords for now...
-NLP_STOPWORDS_EN = os.path.join(PARENT_DIRECTORY, 'data/stopwords_en2.txt')
+NLP_STOPWORDS_EN = os.path.join(PARENT_DIRECTORY, 'resources/misc/stopwords-nlp-en.txt')
 
 DATA_DIRECTORY = '.newspaper_scraper'
 
