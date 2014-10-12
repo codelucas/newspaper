b9dac8d1fbbc05562fe17de9ff26a354dff8b215
Lucas Ou-Yang <lucasyangpersonal@gmail.com>
2014-10-12 16:08:01 -0700

    Revamped all unit tests so that every request is mocked (unit tests will now run for everyone!)
    Deleted a bunch of unused stray data files under /tests/data.

--- tests/benchmarks.py
+++ tests/benchmarks.py
@@ -32,38 +32,43 @@ sys.path.insert(0, os.path.join(PARENT_DIR, '..'))
 from newspaper.network import multithread_request, sync_request
 from newspaper.utils import print_duration
 
+
 @print_duration
 def naive_run(urls):
-    """no multithreading or async io"""
-
+    """no multithreading or async io
+    """
     resps = []
     for url in urls:
         resps.append(sync_request(url))
     print resps
 
+
 @print_duration
 def mthread_run(urls):
-    """download a bunch of urls via multithreading"""
-
-    # print 'beginning batch job for async io, %s threads running' % activeCount()
+    """download a bunch of urls via multithreading
+    """
     reqs = multithread_request(urls)
     resps = [req.resp for req in reqs]
 
+
 @print_duration
 def asyncio_run(urls):
-    """download a bunch of urls via async io"""
-
+    """download a bunch of urls via async io
+    """
     pass
     # rs = (grequests.request('GET', u, **req_kwargs) for u in urls)
     # responses = async_request(urls)
     # print responses
 
+
 def benchmark():
-    """multi-threading vs async-io vs regular"""
-
+    """multi-threading vs async-io vs regular
+    """
     urls = read_urls(amount=1000)
-    #naive_run(urls)
+    # naive_run(urls)
     mthread_run(urls)
-    #asyncio_run(urls)
+    # asyncio_run(urls)
 
-benchmark()
+
+if __name__ == '__main__':
+    benchmark()
