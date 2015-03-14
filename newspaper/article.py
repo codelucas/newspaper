4e5051c77b11ca37b5d81f9386a2812d0eafb9f0
Mehuleo <mehul.marakana@gmail.com>
2015-03-15 00:19:03 +0530

    Adding setting for num of sentences in summary

--- newspaper/article.py
+++ newspaper/article.py
@@ -320,7 +320,9 @@ class Article(object):
         keyws = list(set(title_keyws + text_keyws))
         self.set_keywords(keyws)
 
-        summary_sents = nlp.summarize(title=self.title, text=self.text)
+        max_sents = self.config.MAX_SUMMARY_SENT
+
+        summary_sents = nlp.summarize(title=self.title, text=self.text, max_sents=max_sents)
         summary = '\n'.join(summary_sents)
         self.set_summary(summary)
 
