0ed2d93c6f9ea8cd0f38d01d201970bc74086f05
Yuri Prezument <y@yprez.com>
2016-02-27 17:03:15 +0200

    Some basic tests for title extraction

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -298,6 +298,26 @@ class ArticleTestCase(unittest.TestCase):
         self.assertCountEqual(KEYWORDS, self.article.keywords)
 
 
+class ContentExtractorTestCase(unittest.TestCase):
+    """Test specific element extraction cases"""
+
+    def setUp(self):
+        self.extractor = newspaper.extractors.ContentExtractor(Configuration())
+        self.parser = newspaper.parsers.Parser
+
+    def _get_title(self, html):
+        doc = self.parser.fromstring(html)
+        return self.extractor.get_title(doc)
+
+    def test_get_title_basic(self):
+        html = '<title>Test title</title>'
+        assert self._get_title(html) == 'Test title'
+
+    def test_get_title_split(self):
+        html = '<title>Test page » Test title</title>'
+        assert self._get_title(html) == 'Test title'
+
+
 class SourceTestCase(unittest.TestCase):
     @print_test
     def test_source_url_input_none(self):
