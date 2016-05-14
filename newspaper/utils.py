e9589d321ff667dce910bd8b467c28378681f65c
Parul Sethi <parul1sethi@gmail.com>
2016-05-15 02:29:11 +0530

    Update utils.py

--- newspaper/utils.py
+++ newspaper/utils.py
@@ -351,6 +351,7 @@ def print_available_languages():
         'vi': 'Vietnamese',
         'mk': 'Macedonian',
         'tr': 'Turkish',
+        'hi': 'Hindi'
     }
 
     codes = get_available_languages()
