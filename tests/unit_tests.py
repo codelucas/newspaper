cca00c76d7a892b9fbfc01cd2ca09bc793d8fe5e
Newspad <admin@newspad.me>
2016-08-07 14:58:28 +0900

    Added unit test for 'ja' language

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -578,18 +578,14 @@ class MultiLanguageTestCase(unittest.TestCase):
 
     @print_test
     def test_japanese_fulltext_extract(self):
-        try:
-            url = 'http://www.cnn.co.jp/tech/35087106.html'
-            article = Article(url=url, language='ja')
-            html = mock_resource_with('japanese_article', 'html')
-            article.download(html)
-            article.parse()
-            text = mock_resource_with('japanese', 'txt')
-            self.assertEqual(text, article.text)
-            self.assertEqual(text, fulltext(article.html, 'ja'))
-        except Exception as e:
-            print('type:', type(e))
-            print('error:', e)
+        url = 'http://www.cnn.co.jp/tech/35087106.html'
+        article = Article(url=url, language='ja')
+        html = mock_resource_with('japanese_article', 'html')
+        article.download(html)
+        article.parse()
+        text = mock_resource_with('japanese', 'txt')
+        self.assertEqual(text, article.text)
+        self.assertEqual(text, fulltext(article.html, 'ja'))
 
 
 if __name__ == '__main__':
