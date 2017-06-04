e54ee8a9db253e340aa39fa17c2e2f01f40746e0
codelucas <lucasyangpersonal@gmail.com>
2017-06-04 02:46:27 -0700

    Bump version 0.1.9 => 0.2.0
    New features and bugfixes such as:
    - Python 3.3 deprecation and forward compatibility
    - Feedparser upgrades
    - Readme clarifications
    - New stopwords for Greek

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ with codecs.open('README.rst', 'r', 'utf-8') as f:
 
 setup(
     name='newspaper3k',
-    version='0.1.9',
+    version='0.2.0',
     description='Simplified python article discovery & extraction.',
     long_description=readme,
     author='Lucas Ou-Yang',
