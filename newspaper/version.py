f1b4773754d5242cf9920da257a0984cbe9eeaee
codelucas <lucasyangpersonal@gmail.com>
2017-06-11 03:13:06 -0700

    Bump python2 version

--- newspaper/version.py
+++ newspaper/version.py
@@ -7,5 +7,5 @@ __author__ = 'Lucas Ou-Yang'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014, Lucas Ou-Yang'
 
-version_info = (0, 0, 9, 8)
+version_info = (0, 0, 9, 9)
 __version__ = ".".join(map(str, version_info))
--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='newspaper',
-    version='0.0.9.8',
+    version='0.0.9.9',
     description='Simplified python article discovery & extraction.',
     author='Lucas Ou-Yang',
     author_email='lucasyangpersonal@gmail.com',
