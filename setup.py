8534f390c82f716263731d0e3b86066125945b92
codelucas <lucasyangpersonal@gmail.com>
2017-06-11 03:50:57 -0700

    highlight warning message in python2 install console

--- setup.py
+++ setup.py
@@ -9,13 +9,25 @@ import sys
 import os
 
 
+def hilight(input_string):
+    if sys.stdout.isatty():
+        # only print escape sequences for TTL interfaces
+        return input_string
+    attr = []
+    attr.append('31')  # red
+    attr.append('1')   # bold
+    return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), input_string)
+
+
 # This *must* run early. Please see this API limitation on our users:
 # https://github.com/codelucas/newspaper/issues/155
 if sys.version_info[0] == 3:
-    sys.exit('WARNING! You are attempting to install newspaper\'s '
-             'python2 repository on python3. PLEASE RUN '
-             '`$ pip3 install newspaper3k` for python3 or '
-             '`$ pip install newspaper` for python2')
+    warning_string = hilight(
+        'WARNING! You are attempting to install newspaper\'s '
+        'python2 repository on python3. PLEASE RUN '
+        '`$ pip3 install newspaper3k` for python3 or '
+        '`$ pip install newspaper` for python2')
+    sys.exit(warning_string)
 
 
 try:
@@ -40,7 +52,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='newspaper',
-    version='0.0.9.9',
+    version='0.1.0.0',
     description='Simplified python article discovery & extraction.',
     author='Lucas Ou-Yang',
     author_email='lucasyangpersonal@gmail.com',
