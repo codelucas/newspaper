e7194885762732094ccc79db708849c39712fcd6
codelucas <lucasyangpersonal@gmail.com>
2017-06-13 02:16:04 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ def hilight(input_string):
 
 # This *must* run early. Please see this API limitation on our users:
 # https://github.com/codelucas/newspaper/issues/155
-'''
 if sys.version_info[0] == 3:
     warning_string = hilight(
         'WARNING! You are attempting to install newspaper\'s '
@@ -29,7 +28,6 @@ if sys.version_info[0] == 3:
         '`$ pip3 install newspaper3k` for python3 or '
         '`$ pip install newspaper` for python2')
     sys.exit(warning_string)
-'''
 
 try:
     from setuptools import setup
@@ -50,24 +48,10 @@ packages = [
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
-'''
-
-    package_data = {
-        'newspaper': ['resources/misc/*.txt', 'resources/text/*.txt', 'resources/images/*.txt'],
-    },
-    data_files=[
-        ('newspaper', [
-            'resources/misc/*.txt',
-            'resources/text/*.txt',
-            'resources/images/*.txt',
-        ]),
-    ],
-'''
-
 
 setup(
     name='newspaper',
-    version='0.1.0.1',
+    version='0.1.0.2',
     description='Simplified python article discovery & extraction.',
     author='Lucas Ou-Yang',
     author_email='lucasyangpersonal@gmail.com',
