1dfdde424cb5fc1a58e8c240abce93b3441de101
obulat <obulat@gmail.com>
2017-09-15 21:31:13 +0300

    Special English stopwords loaded

--- newspaper/nlp.py
+++ newspaper/nlp.py
@@ -24,8 +24,15 @@ def load_stopwords(language):
     Loads language-specific stopwords for keyword selection
     """
     global stopwords
-    stopwordsFile = path.join(settings.STOPWORDS_DIR,\
-     'stopwords-{}.txt'.format(language))
+    
+    # stopwords for nlp in English are not the regular stopwords
+    # to pass the tests
+    # can be changed with the tests
+    if language == 'en':
+        stopwordsFile = settings.NLP_STOPWORDS_EN
+    else:
+        stopwordsFile = path.join(settings.STOPWORDS_DIR,\
+                                  'stopwords-{}.txt'.format(language))
     with open(stopwordsFile, 'r', encoding='utf-8') as f:
         stopwords.update(set([w.strip() for w in f.readlines()]))
         
