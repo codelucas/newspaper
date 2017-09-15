e9fc2364d465c82928315bf7b99a31163509669e
Scott Taylor <github@megatron.me.uk>
2017-09-15 17:36:18 +0100

    Do not remove a node if it contains an article (#423)
    Sometimes the cleaner can be a little overzealous and delete a node that contains the main article node. This change may save the article in simple cases.

--- newspaper/cleaners.py
+++ newspaper/cleaners.py
@@ -46,6 +46,7 @@ class DocumentCleaner(object):
             .create("\n", "\n\n")\
             .append("\t")\
             .append("^\\s+$")
+        self.contains_article = './/article|.//*[@id="article"]|.//*[@itemprop="articleBody"]'
 
     def clean(self, doc_to_clean):
         """Remove chunks of the DOM as specified
@@ -119,15 +120,18 @@ class DocumentCleaner(object):
         # ids
         naughty_list = self.parser.xpath_re(doc, self.nauthy_ids_re)
         for node in naughty_list:
-            self.parser.remove(node)
+            if not node.xpath(self.contains_article):
+                self.parser.remove(node)
         # class
         naughty_classes = self.parser.xpath_re(doc, self.nauthy_classes_re)
         for node in naughty_classes:
-            self.parser.remove(node)
+            if not node.xpath(self.contains_article):
+                self.parser.remove(node)
         # name
         naughty_names = self.parser.xpath_re(doc, self.nauthy_names_re)
         for node in naughty_names:
-            self.parser.remove(node)
+            if not node.xpath(self.contains_article):
+                self.parser.remove(node)
         return doc
 
     def remove_nodes_regex(self, doc, pattern):
