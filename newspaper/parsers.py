7de65d2a2cffd34f4cab9a34ebc52353e7bfb657
Harry <yolkmail@gmail.com>
2017-09-23 09:36:12 +0300

    Using simple xpath inside getElementsByTag (#355)
    * Make getElementsByTag much faster and great again!
    
    * Ignore Case
    
    * Refactoring

--- newspaper/parsers.py
+++ newspaper/parsers.py
@@ -12,6 +12,7 @@ import lxml.html
 import lxml.html.clean
 import re
 from html import unescape
+import string
 
 from bs4 import UnicodeDammit
 from copy import deepcopy
@@ -108,13 +109,18 @@ class Parser(object):
 
     @classmethod
     def getElementsByTag(
-            cls, node, tag=None, attr=None, value=None, childs=False) -> list:
-        NS = "http://exslt.org/regular-expressions"
+            cls, node, tag=None, attr=None, value=None, childs=False, use_regex=False) -> list:
+        NS = None
         # selector = tag or '*'
         selector = 'descendant-or-self::%s' % (tag or '*')
         if attr and value:
-            selector = '%s[re:test(@%s, "%s", "i")]' % (selector, attr, value)
-        elems = node.xpath(selector, namespaces={"re": NS})
+            if use_regex:
+                NS = {"re": "http://exslt.org/regular-expressions"}
+                selector = '%s[re:test(@%s, "%s", "i")]' % (selector, attr, value)
+            else:
+                trans = 'translate(@%s, "%s", "%s")' % (attr, string.ascii_uppercase, string.ascii_lowercase)
+                selector = '%s[contains(%s, "%s")]' % (selector, trans, value.lower())
+        elems = node.xpath(selector, namespaces=NS)
         # remove the root node
         # if we have a selection tag
         if node in elems and (tag or childs):
