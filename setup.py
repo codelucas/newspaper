56336255fb94b89c6d76b4029afce14aef68a5ef
Jayson Reis <santosdosreis@gmail.com>
2017-12-22 14:41:40 +0100

    Add a simple client
    Add as simple client in order to download articles and pipe to commands
    like speedread `newspaper -u url | speedread`.

--- setup.py
+++ setup.py
@@ -60,4 +60,8 @@ setup(
         'Natural Language :: English',
         'Intended Audience :: Developers',
     ],
+    entry_points='''
+        [console_scripts]
+        newspaper=newspaper.cli:parse
+    ''',
 )
