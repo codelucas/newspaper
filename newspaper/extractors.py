bdd848e45bf406f3491ac33eb0e5aa4902da0a26
Ask149 <ashishkshirsagar10@gmail.com>
2018-06-08 20:16:46 +0530

    Added some functions of newspaper3k python library

--- newspaper/extractors.py
+++ newspaper/extractors.py
@@ -148,7 +148,7 @@ class ContentExtractor(object):
 
         for match in matches:
             content = ''
-            if match.tag == 'meta':
+            if match.tag :
                 mm = match.xpath('@content')
                 if len(mm) > 0:
                     content = mm[0]
