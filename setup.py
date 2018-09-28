7dc200fa31b81f6317ceff50cc3a9ca6f6c0788e
Lucas Ou-Yang <lucasyangpersonal@gmail.com>
2018-09-27 21:53:09 -0700

    Version bump: 0.2.7 => 0.2.8

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with codecs.open('README.rst', 'r', 'utf-8') as f:
 
 setup(
     name='newspaper3k',
-    version='0.2.7',
+    version='0.2.8',
     description='Simplified python article discovery & extraction.',
     long_description=readme,
     author='Lucas Ou-Yang',
