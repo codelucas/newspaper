43d394a08915985f424b0ad74fa51bb57f21b109
Arthit Suriyawongkul <arthit@gmail.com>
2019-01-21 20:24:13 +0700

    Add Thai stopwords from stopwordsiso

--- newspaper/text.py
+++ newspaper/text.py
@@ -195,3 +195,15 @@ class StopWordsJapanese(StopWords):
         segmenter = tinysegmenter.TinySegmenter()
         tokens = segmenter.tokenize(stripped_input)
         return tokens
+
+
+class StopWordsThai(StopWords):
+    """Thai segmentation
+    """
+    def __init__(self, language='th'):
+        super(StopWordsThai, self).__init__(language='th')
+
+    def candidate_words(self, stripped_input):
+        import pythainlp
+        tokens = pythainlp.word_tokenize(stripped_input)
+        return tokens
